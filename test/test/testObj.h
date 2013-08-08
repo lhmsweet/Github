cd87b090caa8f2a8ad4c291203d9e7957d2f01c1
lhmsweet <lhmsweet1@gmail.com>
2013-08-08 15:32:26 +0800

    add files

---
+++ test/test/testObj.h
@@ -0,0 +1,13 @@
+//
+//  testObj.h
+//  test
+//
+//  Created by lihuaming on 13-8-8.
+//  Copyright (c) 2013年 lihuaming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testObj : NSObject
+
+@end
---
+++ test/test/testObj.m
@@ -0,0 +1,13 @@
+//
+//  testObj.m
+//  test
+//
+//  Created by lihuaming on 13-8-8.
+//  Copyright (c) 2013年 lihuaming. All rights reserved.
+//
+
+#import "testObj.h"
+
+@implementation testObj
+
+@end
