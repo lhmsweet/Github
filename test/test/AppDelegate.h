1ab633783f603b682691d471431a232fe6a754f9
lhmsweet <lhmsweet1@gmail.com>
2013-08-08 15:16:15 +0800

    github

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by lihuaming on 13-8-8.
+//  Copyright (c) 2013年 lihuaming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ test/test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by lihuaming on 13-8-8.
+//  Copyright (c) 2013年 lihuaming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
